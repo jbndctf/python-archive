e834e0ecf166b71a0e682e715f8b45bbc59f21fd
jbndctf <jbndctf@gmail.com>
2025-10-02 09:46:34 +1000

    add docstrings

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,4 +1,16 @@
+"""
+Hello World Program
+
+Prints "Hello, World!" to standard output.
+"""
+
+
 def main() -> None:
+    """
+    Entry point for the program.
+
+    Prints "Hello, World!" to standard output.
+    """
     print("Hello, world!")
 
 
